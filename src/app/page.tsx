a11ac444408a2592da3c4441d23a1ab5769408b1
Uyyam <yumay004@gmail.com>
2024-12-03 17:36:16 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+        <a href= "https://www.linkedin.com/in/may-q-yu/" target="_blank">LinkedIn</a> 
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
+        <h1>Hello, Welcome to My Site</h1>
+        <ul>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            This is a React.js page built with the Next.js framework 
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+          <li>I installed created the app by running the command: npx create-next-app@latest</li>
+          <li>I already had node.js installed from a former project, I updated to version 18.18.0 to use next.js</li>
+          <li>The framework has a lot built in, with a site template that I can use and CSS styling already applied</li>
+          <li>I appreciate the component based structure of React and like the ease of the next framework for developing a page</li>
+        </ul><br></br>
+        <p>The site is hosted on my personal GitHub through GitHub pages: <a href="https://github.com/Uyyam" target="_blank">My GitHub Account</a></p>
+        <Image src="/chopper.png" alt="My Headshot" width={300} height={300}></Image>
+        
+        
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
